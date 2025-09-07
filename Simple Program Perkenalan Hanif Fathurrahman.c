9a47da69a35adc30b858decf30261ab7e60df5d7
Hanif <hanifwica@gmail.com>
2025-09-07 11:15:06 +0700

    add Introduction program

---
+++ Simple Program Perkenalan Hanif Fathurrahman.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for(int x=0; x<=4 ; x++)
+    {
+        if (x==0) printf("Hai\n");
+        else if (x==1) printf("Nama saya Hanif Fathurrahman Wicaksono\n");
+        else if (x==2) printf("Saya seorang mahasiswa jurusan Teknik Informatika, Universitas Bina Nusantara\n");
+        else if (x==3) printf("Bahasa prmograman yang saya suka adalah C#, C++, dan C\n");
+        else if (x==4) printf("Itu saja perkenalan dari saya, Terima kasih\n");
+    }
+    return 0;
+}
